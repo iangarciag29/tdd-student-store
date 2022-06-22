eb5d493e65320dba447f10ace8d45ff00e60cad4
Ian <iangarciag@fb.com>
2022-06-22 15:54:51 -0700

    Added missing store routes.

--- student-store-ui/src/components/Home/Home.jsx
+++ student-store-ui/src/components/Home/Home.jsx
@@ -1,10 +1,28 @@
 import * as React from "react"
 import "./Home.css"
+import Navbar from "../Navbar/Navbar";
+import Footer from "../Footer/Footer";
+import ToTopBtn from "../ToTop/ToTopBtn";
+import {Route, Routes} from "react-router-dom";
+import Landing from "../../pages/Landing";
+import Payment from "../../pages/Payment";
+import Products from "../../pages/Products";
+import Product from "../../pages/Product";
 
 export default function Home() {
-  return (
-    <div className="home">
-      <p>Home</p>
-    </div>
-  )
+    return (
+        <div className="w-5/6 bg-gray-50 flex flex-col">
+            <Navbar/>
+            <div className="overflow-y-auto py-32 px-10">
+                <Routes>
+                    <Route path="/" element={<Landing/>}/>
+                    <Route path="/products" element={<Products/>}/>
+                    <Route path="/product/:id" element={<Product/>}/>
+                    <Route path="/payment" element={<Payment/>}/>
+                </Routes>
+            </div>
+            <Footer/>
+            <ToTopBtn/>
+        </div>
+    )
 }
