a4aac2fe306d908d225a0180412384b5907d0128
Ian <iangarciag@fb.com>
2022-06-22 15:52:27 -0700

    Added some required icons.

---
+++ student-store-ui/src/icons/MinusIcon.jsx
@@ -0,0 +1,6 @@
+export const MinusIcon = () => {
+    return <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24"
+                stroke="currentColor" strokeWidth={2}>
+        <path strokeLinecap="round" strokeLinejoin="round" d="M18 12H6"/>
+    </svg>;
+}
