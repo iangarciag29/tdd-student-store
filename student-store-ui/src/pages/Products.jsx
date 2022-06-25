0e2e99bd7737de58bc047cec5f8beb143cb111be
Ian <iangarciag@fb.com>
2022-06-24 17:09:49 -0700

    Implemented add to cart action.

--- student-store-ui/src/pages/Products.jsx
+++ student-store-ui/src/pages/Products.jsx
@@ -2,6 +2,9 @@ import {useEffect, useState} from "react";
 import axios from "axios";
 import {API_URL} from "../api";
 import {ListGroup} from "flowbite-react";
+import useAppError from "../Hooks/useAppError";
+import {Loader} from "../utils/Loader";
+import useShoppingCart from "../Hooks/useShoppingCart";
 
 const categories = [
     {
@@ -28,18 +31,28 @@ const categories = [
 
 const Products = () => {
 
+    const {addError} = useAppError();
+    const {addItem} = useShoppingCart();
     const [selectedCategory, setSelectedCategory] = useState("all");
     const [products, setProducts] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         (async () => {
             await axios.get(API_URL).then(res => {
-                setProducts(res.data.products);
+                setTimeout(() => {
+                    setProducts(res.data.products);
+                    setIsLoading(false);
+                }, 500);
             }).catch(err => {
-                console.error(err);
+                addError(err.code, err.response.status, err.response.statusText);
             });
         })();
     }, []);
+
+    if (isLoading) return <div className="text-center mt-24">
+        <Loader/>
+    </div>
 
     return <div className="container">
         <div className="w-full pb-10 text-center">
@@ -60,10 +73,29 @@ const Products = () => {
                     )}
                 </ListGroup>
             </div>
-            <div className="w-5/6 p-10 grid grid-cols-3">
-                {products?.filter(product => (selectedCategory === "all" ? 1 : product.category === selectedCategory)).map((product, idx) =>
-                    <div key={idx}>
-                        {product.name}
+            <div className="w-5/6 p-10 grid grid-cols-3 gap-5">
+                {products?.filter(product => (selectedCategory === "all" ? 1 : product.category === selectedCategory)).map(product =>
+                    <div className="max-w-sm bg-white rounded-lg shadow-md"
+                         key={product.id}>
+                        <img className="p-8 rounded-t-lg" src={product.image}
+                             alt={product.name}/>
+                        <div className="px-5 pb-5">
+                            <a href="#">
+                                <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{product.name}</h5>
+                            </a>
+                            <div className="flex items-center mt-2.5 mb-5">
+                                <p>{product.description}</p>
+                            </div>
+                            <div className="flex justify-between items-center">
+                                <span
+                                    className="text-3xl font-bold text-gray-900 dark:text-white">${product.price}</span>
+                                <button
+                                    onClick={() => addItem(product)}
+                                    className="text-white bg-blue-900 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Add
+                                    to cart
+                                </button>
+                            </div>
+                        </div>
                     </div>
                 )}
             </div>
