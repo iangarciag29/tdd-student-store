6023498fca3b4723fde9677b0d2f6bb57bb30672
Ian <iangarciag@fb.com>
2022-06-24 17:10:59 -0700

    Wrapped application within the provider.

--- student-store-ui/src/components/App/App.jsx
+++ student-store-ui/src/components/App/App.jsx
@@ -3,18 +3,21 @@ import Home from "../Home/Home"
 import {BrowserRouter} from "react-router-dom";
 import Sidebar from "../Sidebar/Sidebar";
 import {ErrorProvider} from "../Context/ErrorProvider";
+import {CartProvider} from "../Context/CartProvider";
 
 export default function App() {
     return (
         <ErrorProvider>
-            <div className="app">
-                <BrowserRouter>
-                    <main className="w-full min-h-screen flex flex-row relative md:fixed inset-0">
-                        <Sidebar/>
-                        <Home/>
-                    </main>
-                </BrowserRouter>
-            </div>
+            <CartProvider>
+                <div className="app">
+                    <BrowserRouter>
+                        <main className="w-full min-h-screen flex flex-row relative md:fixed inset-0">
+                            <Sidebar/>
+                            <Home/>
+                        </main>
+                    </BrowserRouter>
+                </div>
+            </CartProvider>
         </ErrorProvider>
     )
 }
