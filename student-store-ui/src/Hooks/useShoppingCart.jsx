78b6ae68151b01ddf4971fc3690adbae3f61b9cf
Ian <iangarciag@fb.com>
2022-06-27 17:56:20 -0700

    Updated hook to improve variable naming.

--- student-store-ui/src/Hooks/useShoppingCart.jsx
+++ student-store-ui/src/Hooks/useShoppingCart.jsx
@@ -2,8 +2,8 @@ import {useContext} from "react";
 import {CartContext} from "../components/Context/CartProvider";
 
 const useShoppingCart = () => {
-    const {items, total, quantity, addItem, removeItem, deleteItem} = useContext(CartContext);
-    return {items, total, quantity, addItem, removeItem, deleteItem};
+    const {cartItems, total, quantity, addItem, removeItem, deleteItem} = useContext(CartContext);
+    return {cartItems, total, quantity, addItem, removeItem, deleteItem};
 }
 
 export default useShoppingCart;
