b414495c5ea857b31b876657efbcc312490c821e
Ian <iangarciag@fb.com>
2022-07-01 22:16:02 -0700

    Documented storage handler.

--- student-store-express-api/data/storage.js
+++ student-store-express-api/data/storage.js
@@ -2,27 +2,44 @@ const path = require("path")
 const low = require("lowdb")
 const FileSync = require("lowdb/adapters/FileSync")
 
+/**
+ * Application storage handler.
+ */
 class Storage {
-    constructor() {
-        this.path = path.resolve(__dirname, "db.json")
-        this.setup()
-    }
+  constructor() {
+    this.path = path.resolve(__dirname, "db.json")
+    this.setup()
+  }
 
-    async setup() {
-        const adapter = new FileSync(this.path)
-        this.db = low(adapter)
-        this.db.defaults({purchases: [], products: []}).write()
-    }
+  /**
+   * Initial method for storage setup.
+   */
+  async setup() {
+    const adapter = new FileSync(this.path)
+    this.db = low(adapter)
+    this.db.defaults({ purchases: [], products: [] }).write()
+  }
 
-    add(key, value) {
-        return this.db.get(key).push(value).write();
-    }
+  /**
+   * Stores value into local database.
+   * @param {string} key Storage key
+   * @param {string} value Storage value
+   * @returns value
+   */
+  set(key, value) {
+    return this.db.set(key, value)
+  }
 
-    get(key) {
-        return this.db.get(key).value();
-    }
+  /**
+   * Method to retreive data.
+   * @param {string} key Value to retreive data from local database.
+   * @returns Value
+   */
+  get(key) {
+    return this.db.get(key)
+  }
 }
 
 module.exports = {
-    storage: new Storage(),
+  storage: new Storage(),
 }
